05a3d28a788f67b50e2d43d972a21992706bf8e3
Chilaka Chinedu <greatchinex@gmail.com>
2019-07-20 20:41:15 +0100

    Added basic Schema and resolver

--- server.js
+++ server.js
@@ -1,12 +1,47 @@
 import express from "express";
-import expressGraphQL from "express-graphql";
+import graphqlHTTP from "express-graphql";
 import dotenv from "dotenv";
+import { buildSchema } from "graphql";
 
 const app = express();
 dotenv.config();
-
 const port = process.env.PORT;
+app.use(express.json()); // Express has its own body parser so no need 2 use d body-parser package
+
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema: buildSchema(`
+            type RootQuery {
+                events: [String!]!
+            }
+
+            type RootMutation {
+                createEvent(name: String): String
+            }
+
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+            events: () => {
+                return [
+                    "Developers MeetUp",
+                    "Employees Get Together",
+                    "All Night Coding"
+                ];
+            },
+            createEvent: args => {
+                const eventName = args.name;
+                return eventName;
+            }
+        },
+        graphiql: true
+    })
+);
 
 app.listen(port, () => {
-  console.log(`Server is Listening on Port ${port}`);
+    console.log(`Server is Listening on Port ${port}`);
 });
